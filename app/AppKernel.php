cb8573301aa71c6240e46711bf803723bd75f66b
Brian Zitzow <bzitzow@gmail.com>
2013-04-23 21:42:25 -0700

    New bundle, NewParadymeUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new NewParadyme\UserBundle\NewParadymeUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/NewParadyme/UserBundle/NewParadymeUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NewParadyme\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NewParadymeUserBundle extends Bundle
+{
+}
