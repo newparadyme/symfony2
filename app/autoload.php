3fc12c281f2b72329dd25be0ab4dae2e24e827c1
Brian Zitzow <bzitzow@gmail.com>
2013-04-25 15:48:25 -0700

    Autoload MongoODMBundle and Register namespace in AppKernel.php

--- app/autoload.php
+++ app/autoload.php
@@ -1,6 +1,7 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
@@ -9,6 +10,7 @@ if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 }
 
+AnnotationDriver::registerAnnotationClasses();
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
