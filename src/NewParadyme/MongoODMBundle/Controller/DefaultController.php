0155f71ba4a0b0be8883c00787316f4cc3d94342
Brian Zitzow <bzitzow@gmail.com>
2013-04-25 15:49:46 -0700

    Example insert test Product into mongodb.

--- src/NewParadyme/MongoODMBundle/Controller/DefaultController.php
+++ src/NewParadyme/MongoODMBundle/Controller/DefaultController.php
@@ -5,15 +5,33 @@ namespace NewParadyme\MongoODMBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use NewParadyme\MongoODMBundle\Document\Product;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/mongodb/sample")
+     * @Route("/mongodb")
      * @Template()
      */
     public function indexAction()
     {
         return array();
+    }
+
+    /**
+     * @Route("/mongodb/create")
+     */
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $dm->persist($product);
+        $dm->flush();
+
+        return new Response('Created product id '.$product->getId());
     }
 }
